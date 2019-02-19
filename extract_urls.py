8b56fa4f18e6d51caad1e6e2123ae0cde4f14ed1
Joshua Peterson <peterson.c.joshua@gmail.com>
2019-02-18 23:34:36 -0500

    small fixes

--- extract_urls.py
+++ extract_urls.py
@@ -1,26 +1,34 @@
 import os, sys, json, argparse
-from collections import Counter
 
 from utils import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--psdir', type=str, default='pushshift_dumps')
+parser.add_argument('--outdir', type=str, default='url_dumps')
 parser.add_argument('--year_start', type=int, default=2018)
 parser.add_argument('--year_end', type=int, default=2018)
+parser.add_argument('--single_file', type=str, default=None)
 parser.add_argument('--min_karma', type=int, default=3)
+parser.add_argument('--dont_compress', action='store_true', default=False)
 args = parser.parse_args()
 
+# for processing many pushshift dumps at once
 filenames = []
-years = range(args.year_start, args.year_end+1)
-years = [str(year) for year in years]
-for fn in os.listdir(args.psdir):
-    for year in years:
-        if year in fn: 
-            filenames.append(fn)
-filenames = sorted(filenames)
-print('Processing the following files', 
-      filenames)
+if args.single_file is None:
+    years = range(args.year_start, args.year_end+1)
+    years = [str(year) for year in years]
+    for fn in os.listdir(args.psdir):
+        for year in years:
+            if year in fn: 
+                filenames.append(fn)
+    filenames = sorted(filenames)
+    print('Processing the following files', 
+          filenames)
+else:
+    # args.single_file overrides the year range
+    filenames.append(args.single_file) 
 
+# extract all good links and save
 good_links = []
 for fn in filenames:
     
@@ -31,7 +39,8 @@ for fn in filenames:
     decompress = get_decompresser(fn)
 
     with decompress(path, "r") as psfile:
-        with open(fn+'.goodlinks.txt', 'w') as outfile:
+        out_path = os.path.join(args.outdir, fn+'.goodlinks.txt')
+        with open(out_path, 'w') as outfile:
 
             for line in psfile:
 
@@ -42,13 +51,20 @@ for fn in filenames:
                    (j['score'] > args.min_karma-1) and \
                    (not j['over_18']):
 
+                    # save the good url
                     outfile.write(j['url'] + '\n')
 
+                    # some basic logging
                     hit_count += 1
                     if hit_count % 10000==0:
                         print(hit_count, total_count, hit_count/float(total_count))
+                        # flush the output every now and then
                         outfile.flush()
-                        os.system('xz -zkf '+fn+'.goodlinks.txt')
                 total_count += 1
 
+# save a compressed version too
+if not args.dont_compress:
+    os.system('xz -zkf '+out_path)
+
 print(hit_count, total_count, hit_count/float(total_count))
+print('Done!')
