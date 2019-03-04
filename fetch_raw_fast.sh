da91fcbe212d4035b84e6dcf949e509660ab4465
Simon Fallnich <simon@fallnich.net>
2019-03-04 16:30:20 +0100

    Added fast fetching tool

--- fetch_raw_fast.sh
+++ fetch_raw_fast.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-mkdir raw_dump && cd raw_dump
-cat $(ls ../urldumps | grep ".deduped.txt") | xargs curl $1 -o $1-$(date +"%T").html
+
+export MAX_THREADS=100
+
+main() {
+	mkdir -p raw_dumps && cd url_dumps
+	cat $(ls -d $PWD/* | grep ".deduped.txt") | sed -E "p;s/[\/:]/-/g" | paste -d ' ' - - | xargs -n 2 -P $MAX_THREADS -l bash -c 'curl -o ../raw_dumps/$1-$(date | tr " " "-" | tr ":" "_").html $0'
+}
+
+# Execute main function
+main
