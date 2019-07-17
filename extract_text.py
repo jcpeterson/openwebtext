26dd3f0b6f3d8ff3f917645600d469a166fd9de0
Johannes Villmow <villmow@supergpu.local.cs.hs-rm.de>
2019-07-17 12:21:27 +0200

    cleanup

--- extract_text.py
+++ extract_text.py
@@ -1,17 +1,18 @@
 from __future__ import print_function
 from __future__ import division
 
-from glob import glob
-import os.path as op
 import argparse, time, tarfile
+from glob import glob
 from hashlib import md5
 import multiprocessing as mpl
+import os.path as op
 import pathlib as pl
 
 import newspaper
 from tqdm import tqdm
 
 from utils import mkdir, chunks, extract_month
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--html_archive", type=str, default="scraped/RS_2017-04-4_data.xz")
@@ -44,25 +45,25 @@ def get_processed_files(out_dir):
 
 def parse_archive(archive_fp, out_dir, n_procs, chunk_size=100):
     tmp_data_dir = pl.Path(archive_fp).with_suffix(".tmp")
-    
+
     # extract tar first
     if not tmp_data_dir.exists():
         tar = tarfile.open(archive_fp)
         tar.extractall(tmp_data_dir)
         tar.close()
-    
 
+    # get files to process
     processed_files = set(get_processed_files(out_dir))
     num_total_files = len([_ for _ in tmp_data_dir.iterdir()])
     num_remaining_files = num_total_files - len(processed_files)
     print("{}/{} files already processed.".format(len(processed_files), num_total_files))
-    
+
     def file_gen():
         for filename in tmp_data_dir.iterdir():
             if filename.name not in processed_files and filename.is_file():
                 yield filename
-    
-    out_dir = pl.Path(out_dir) 
+
+    out_dir = pl.Path(out_dir)
     unparsable = 0
 
     if n_procs == 1:
@@ -83,7 +84,7 @@ def parse_archive(archive_fp, out_dir, n_procs, chunk_size=100):
 
                 save_parsed_file(filename, text, out_dir)
     print("Could not parse {} files".format(unparsable))
-    
+
 
 if __name__ == "__main__":
     month = extract_month(args.html_archive)
